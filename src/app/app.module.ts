bc057f939864fa3617a9aee9fb67083aacb8d960
DESKTOP-TTUFU5F\PC <milos.novakovic.serbia.5@gmail.com>
2023-01-11 14:11:57 +0100

    adding basics for the angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+/* essential for app */
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyCW4ooGl02kqPUteyqs5hZWAa6ELCUVDYs",
+  authDomain: "angularsertifikat5deo.firebaseapp.com",
+  databaseURL: "https://angularsertifikat5deo-default-rtdb.firebaseio.com",
+  projectId: "angularsertifikat5deo",
+  storageBucket: "angularsertifikat5deo.appspot.com",
+  messagingSenderId: "370457630259",
+  appId: "1:370457630259:web:9d998185229e06ab133546"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
